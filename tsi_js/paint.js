75811d03b902c06fb3bac704a3cf3dc240b7d5ca
Abusaleh Masud <37705766+randomblue24@users.noreply.github.com>
2024-05-16 11:47:20 -0400

    created function to get paint

--- tsi_js/paint.js
+++ tsi_js/paint.js
@@ -1,5 +1,37 @@
-function getInput(callback) {
-    var input = prompt("Enter a number:");
-    callback(input);
+//importing promptsync to handle user input output
+const prompt = require("prompt-sync")();
+
+//function to get paint price
+function getPaint(text){
+    let white=20;
+    let black=25;
+    let blue=30;
+    let red=35;
+    let green=40;
+  
+    //using switch case to return correct price
+    switch (text) {
+        case "white":
+            return white;
+        case "blue":
+            return blue;
+        case "green":
+            return green;
+        case "red":
+            return red;
+        case "black":
+            return black;
+        default:
+            return;
+    }
 }
+
+function getInput() {
+    //note, input is a function in js
+    let input = prompt("Enter a number:");
+    return input;
+}
+
+console.log(getInput());
+
 
