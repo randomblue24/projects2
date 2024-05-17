224f2dfed1e55d6c0bbb94b3d43a67b63e2a3180
Abusaleh Masud <37705766+randomblue24@users.noreply.github.com>
2024-05-17 07:14:20 -0400

    Almost finished, added functionality to choose number of walls and two types of walls

--- tsi_js/paint.js
+++ tsi_js/paint.js
@@ -8,7 +8,10 @@ function paintPrice(text){
     const blue=30;
     const red=35;
     const green=40;
-  
+
+    let regularPaint = {white: 20, black: 25,blue: 30,red: 35,green: 40 };
+    let premiumPaint= {white: 25, black: 30, blue: 35,red: 40,green: 45 };
+
     //using switch case to return correct price
     switch (text) {
         case "white":
@@ -28,16 +31,61 @@ function paintPrice(text){
 
 function getPaint() {
     //note, input is a function in js
-    console.log("White: $20 /liter, \nBlack: $25 /liter \nBlue: $30 /liter \nRed: $35 /liter \nGreen: $40 /liter");
-    let input = prompt("Please choose paint: ").toLowerCase();
+    console.log("Regular: White: $20 /liter, \nBlack: $25 /liter \nBlue: $30 /liter \nRed: $35 /liter \nGreen: $40 /liter");
+    console.log("Premium: White: $25 /liter, \nBlack: $30 /liter \nBlue: $35 /liter \nRed: $40 /liter \nGreen: $45 /liter");
+    
+    let input = prompt("Please choose Regular or Premium Paint: ").toLowerCase();
+
+    //while loop to ensure correct input and stop users from entering invalid inputs
+    while (input!=="regular" &&  input!=="premium") {
+        input=prompt("Invalid choice, Please choose again: ".toLowerCase());
+    }
+
+    input = prompt("Please choose paint color: ").toLowerCase();
     //while loop to ensure correct input and stop users from entering invalid inputs
     while (input!=="red" &&  input!=="green" && input!=="blue" && input!=="white" && input!=="black") {
         input=prompt("Invalid choice, Please choose again: ".toLowerCase());
     }
+    
     return input;
 
 }
 
+//Let the user provide how many walls and measurements of said walls
+function getarea(){
+    let walls=0;
+    while(walls==0){
+        walls=prompt("How many walls do you need? ");
+        if(walls==0){
+            console.log("Please enter a number greater than 0");
+        }
+    }
+    let typeofwall;
+    let total_squareft=0;
+    let base=0;
+    let height=0;
+
+    for (let i=0; i<walls; i++){
+        typeofwall=prompt("What type of wall is it: Rectangle, Triangle: ");
+        typeofwall=typeofwall.toLowerCase();
+        switch (typeofwall) {
+            case "rectangle":
+                base=prompt("Measurement of base wall: ");
+                height=prompt("Height of wall: ");
+                total_squareft=total_squareft+(base*height);
+
+                break;
+            
+            case "triangle":
+                width=prompt("Width of base of wall: ");
+                height=prompt("Height of wall: ");
+                total_squareft=total_squareft+((width*height)/2);
+
+                break;
+        }
+    }
+    return total_squareft;
+}
 //function to get the price and liters of paint needed
 function paintAmount(priceperliter, measurement){
     //1 liter = 100 square feet
@@ -82,7 +130,7 @@ function main(){
     customerChoice(color, price);
 
     //get the amount of paint needed
-    let measurement=prompt("How many square feet of paint do you need? ");
+    let measurement=getarea();
 
     //this doen't work because it's returning an object, and need to access the values as well. So how?
     //let total_price, total_liters=paintAmount(price, measurement);
@@ -92,7 +140,7 @@ function main(){
 
     console.log ("Your total is $" + total_price + " for " + total_liters + " liters of " + color + " paint");
 
+}
+
 main();
-
-
 
